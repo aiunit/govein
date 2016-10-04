2f6eeb44aa8452df13b8ee9becaa88dfcc84fd9b
Alfred Huang <57082212@qq.com>
2016-10-04 20:43:17 +0800

    Label and history added.

--- core/static/core/js/script.js
+++ core/static/core/js/script.js
@@ -10,9 +10,24 @@
     window.app = new Vue({
         el: '#app',
         data: {
-            auto: true,
+            label:{
+                x: [
+                    19, 18, 17, 16,
+                    15, 14, 13, 12, 11,
+                    10, 9, 8, 7, 6,
+                    5, 4, 3, 2, 1
+                ],
+                y: 'ABCDEFGHJKLMNOPQRST'
+            },
+            autoSwitch: true,
             player: 1,
-            grid: grid
+            grid: grid,
+            rob: {
+                x: -1,
+                y: -1
+            },
+            history: [],
+            history_cursor: -1
         },
         methods: {
             draw: function () {
@@ -25,17 +40,72 @@
                 }
                 console.log(str);
             },
-            go: function (r, c) {
-                console.log('go(' + r + ', ' + c + ')');
-                if (this.grid[r][c]) return false;
-                this.grid[r][c] = this.player;
-                this.grid[r].splice(c, 1, this.player);
-                if (this.auto) this.player = 3 - this.player;
+            prev: function () {
+                if (this.history_cursor < 0) return false;
+                var history = this.history[this.history_cursor--];
+                this.go(
+                    history.x,
+                    history.y,
+                    history.old_val,
+                    true
+                );
+            },
+            next: function () {
+                if (this.history_cursor >= this.history.length - 1) return false;
+                var history = this.history[++this.history_cursor];
+                this.go(
+                    history.x,
+                    history.y,
+                    history.new_val,
+                    true
+                );
+            },
+            go: function (x, y, action, nolog) {
+
+                console.log('go(' + x + ', ' + y + ')');
+
+                if (typeof(action) === 'undefined') {
+                    action = parseInt(this.player);
+                }
+
+                if (action && this.grid[x][y]) {
+                    return false;
+                }
+
+                var log = {
+                    x: x,
+                    y: y,
+                    old_val: this.grid[x][y],
+                    new_val: action
+                };
+
+                this.grid[x].splice(y, 1, action);
+
+                if (!nolog) {
+
+                    if (action && this.autoSwitch) {
+                        this.player = 3 - action;
+                    }
+
+                    this.history.splice(
+                        this.history_cursor,
+                        this.history.length - 1 - this.history_cursor
+                    );
+
+                    this.history.push(log);
+
+                    this.history_cursor = this.history.length - 1;
+
+                }
+
                 this.draw();
             },
-            isStar: function (r, c) {
-                return (r == 3 || r == 9 || r == 15)
-                    && (c == 3 || c == 9 || c == 15);
+            isStar: function (x, y) {
+                return (x == 3 || x == 9 || x == 15)
+                    && (y == 3 || y == 9 || y == 15);
+            },
+            applyCapture: function () {
+
             }
         }
     });
