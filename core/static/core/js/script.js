2ab744443fc3339578183b675f88bdda609a81ca
Alfred Huang <57082212@qq.com>
2016-10-04 19:40:40 +0800

    Complete the placing stone functional.

--- core/static/core/js/script.js
+++ core/static/core/js/script.js
@@ -15,6 +15,24 @@
             grid: grid
         },
         methods: {
+            draw: function () {
+                var str = '';
+                for (var i = 0; i < this.grid.length; ++i) {
+                    for (var j = 0; j < this.grid[i].length; ++j) {
+                        str += '.xo'[this.grid[i][j]];
+                    }
+                    str += '\n';
+                }
+                console.log(str);
+            },
+            go: function (r, c) {
+                console.log('go(' + r + ', ' + c + ')');
+                if (this.grid[r][c]) return false;
+                this.grid[r][c] = this.player;
+                this.grid[r].splice(c, 1, this.player);
+                if (this.auto) this.player = 3 - this.player;
+                this.draw();
+            },
             isStar: function (r, c) {
                 return (r == 3 || r == 9 || r == 15)
                     && (c == 3 || c == 9 || c == 15);
